b2ffbdb2175c24d46a639356e5af255a3d000d6f
Safal Baral <130429214+safalbaral@users.noreply.github.com>
2024-06-15 16:15:43 +0545

    Completed 4.2

---
+++ part4/bloglist/app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => logger.info('Connected to MongoDB!'))
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
+
+
+
