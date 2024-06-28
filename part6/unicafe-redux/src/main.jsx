23aa2d04cab77b2910ec16eaaf977da9163ba8f7
Safal Baral <safalbaral@Safals-MacBook-Pro.local>
2024-06-28 11:39:13 +0545

    Completed 6.2

--- part6/unicafe-redux/src/main.jsx
+++ part6/unicafe-redux/src/main.jsx
@@ -7,21 +7,21 @@ import reducer from './reducer'
 const store = createStore(reducer)
 
 const App = () => {
-  const good = () => {
+  const ratingDispatcher = (rating_type) => {
     store.dispatch({
-      type: 'GOOD'
+      type: rating_type
     })
   }
 
   return (
     <div>
-      <button onClick={good}>good</button> 
-      <button>ok</button> 
-      <button>bad</button>
-      <button>reset stats</button>
+      <button onClick={() => ratingDispatcher('GOOD')}>good</button> 
+      <button onClick={() => ratingDispatcher('OK')}>ok</button> 
+      <button onClick={() => ratingDispatcher('BAD')}>bad</button>
+      <button onClick={() => ratingDispatcher('ZERO')}>reset stats</button>
       <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
     </div>
   )
 }
