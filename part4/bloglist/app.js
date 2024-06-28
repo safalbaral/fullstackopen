1de57180735324ef9def7a11fd6cb749dbb91f0a
Safal Baral <130429214+safalbaral@users.noreply.github.com>
2024-06-28 11:30:01 +0545

    Completed 6.1

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const logger = require('./utils/logger')
 
 mongoose.connect(config.MONGODB_URI)
@@ -13,6 +14,7 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
 
