c7f257fd28a68b69b107a65c1f10fb17fa7c5f25
Safal Baral <130429214+safalbaral@users.noreply.github.com>
2024-06-29 15:59:19 +0545

    Implemented 6.13 with useEffect instead

--- part6/redux-anecdotes/src/components/Notification.jsx
+++ part6/redux-anecdotes/src/components/Notification.jsx
@@ -1,8 +1,18 @@
 import { useSelector } from "react-redux"
+import { useEffect } from "react"
+import { useDispatch } from "react-redux"
+import { removeNotification } from "../reducers/notificationReducer"
 
 const Notification = () => {
   const notification = useSelector(state => state.notification)
-  
+  const dispatch = useDispatch()
+
+  useEffect(()=> {
+    setTimeout(()=> {
+      dispatch(removeNotification())
+    }, 5000)
+  }, [dispatch, notification])
+
   const style = {
     border: 'solid',
     padding: 10,
