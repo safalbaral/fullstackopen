2979820c1ad4247e5459e9735da4cfc44eddabf4
Safal Baral <safalbaral@Safals-MacBook-Pro.local>
2024-06-29 15:50:10 +0545

    Completed upto 6.13

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -1,6 +1,7 @@
 import { useSelector, useDispatch } from 'react-redux'
 import { vote } from '../reducers/anecdoteReducer'
 import { sortByVotes } from '../reducers/anecdoteReducer'
+import { sendNotification, removeNotification } from '../reducers/notificationReducer'
 
 const AnecdoteList = () => {
 
@@ -8,6 +9,14 @@ const AnecdoteList = () => {
                                                                 sortByVotes(state.anecdotes)
                                                                :sortByVotes(state.anecdotes.filter(anecdote => anecdote.content.includes(state.filter))))
     const dispatch = useDispatch()
+
+    const voteAndNotificationDispatcher = (anecdote) => {
+      dispatch(vote(anecdote.id))
+      dispatch(sendNotification(`You voted for ${anecdote.content}`))
+      setTimeout(()=> {
+        dispatch(removeNotification())
+      }, 5000)
+    }
 
     return(
         <div>
@@ -18,7 +27,7 @@ const AnecdoteList = () => {
               </div>
               <div>
                 has {anecdote.votes}
-                <button onClick={() => dispatch(vote(anecdote.id))}>vote</button>
+                <button onClick={() => voteAndNotificationDispatcher(anecdote)}>vote</button>
               </div>
             </div>
           )}
